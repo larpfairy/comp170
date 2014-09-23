d743b32ace238856af2cdf37ae790d0df7c48cd9
larpfairy <michaelgibs123@gmail.com>
2014-09-23 02:00:03 -0500

    added files for class

---
+++ for_each.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class ForEach
+{
+   static void Main()
+   {
+      string s = "0123456789";
+      foreach (char number in s)
+      {
+         if (number % 2 == 0) {
+            Console.WriteLine (number);
+         }
+      }
+
+   }
+}
